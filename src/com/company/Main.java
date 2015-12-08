ff7ab229a77541c4e753eb4d02e8d1b25aae3d15
Artem Nerezhko <cloxteam@gmail.com>
2015-12-09 01:05:15 +0200

    Third Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Fuck");
-        System.out.println("Fuck");
-        System.out.println("Fuck");
-        System.out.println("Fuck");
+        // TODO Auto-generated method stub
+        if (args.length == 0)
+        {
+            System.err.print("Ошибочка надо что то ввести!");
+        }
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext())
+        {
+            String str = in.nextLine();
+            Scanner s = new Scanner(str);
+            for (String iterStr: args)
+                if ((str.toLowerCase().contains(iterStr.toLowerCase())) ||
+                        (s.findInLine(iterStr) != null))
+                {
+                    System.out.println(str);
+                    break;
+                }
+            s.close();
+
+        }
+        in.close();
     }
-}
+    }
+
